5a21ee33addfcdbc24a99f272861855e32efe742
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-09-04 17:02:38 +0200

    repo init

---
+++ src/firebase.config.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDM3prYnos2DKUZFXMCMcn7LVGkRwjaAU",
+  authDomain: "boitealire-39fde.firebaseapp.com",
+  projectId: "boitealire-39fde",
+  storageBucket: "boitealire-39fde.appspot.com",
+  messagingSenderId: "147444454910",
+  appId: "1:147444454910:web:4e19566a8cf2ebf19f9caf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
