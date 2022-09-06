a0dcb93ff430b7d485072f073f48f115453c442f
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-09-06 11:05:28 +0200

    ADDED logout method in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,21 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/userContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase-config";
 // import { INavbar, IAppContext } from "../@Types/lectures";
 
 export default function Navbar() {
   const { toggleModals } = useContext(UserContext);
+  const navigate = useNavigate();
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch {
+      alert("For some reasons we can't log you out. Please retry later.");
+    }
+  };
   return (
     <nav className="navbar navbar-light bg-light px-4">
       <Link to="/" className="navbar-brand">
@@ -23,7 +34,9 @@ export default function Navbar() {
         >
           Sign In
         </button>
-        <button className="btn btn-danger ms-2">Logout</button>
+        <button onClick={logOut} className="btn btn-danger ms-2">
+          Logout
+        </button>
       </div>
     </nav>
   );
