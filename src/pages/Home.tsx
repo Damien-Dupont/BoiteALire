eb8443db08e418e8ce57d5a432568c6b5e1b4ecb
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-09-06 11:30:23 +0200

    COMPLETED logout method

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/userContext";
 
 export interface IHomePageProps {}
 
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
+  const { currentUser } = useContext(UserContext);
+  const message: string =
+    "If you already have an account, please sign in. If you don't, sign up!";
   return (
     <div className="container p-5">
-      <h1 className="display-3 text-light">Home</h1>
+      <h1 className="display-3 text-light">
+        {currentUser ? `Welcome buddy` : `Welcome! ` + message}
+      </h1>
     </div>
   );
 };
