d59af6b90fe63e8cff5df52cf8d0d8ab8efbaa64
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-09-22 15:11:19 +0200

    ADDED commentList for any book

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import {
   doc,
@@ -15,6 +15,9 @@ import {
   setDoc,
 } from "firebase/firestore";
 import { db } from "../firebase-config";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+import BookCard from "../components/BookCard";
 // import { useFirestoreDocument } from "@react-query-firebase/firestore";
 // import { getAutoHeightDuration } from "@mui/material/styles/createTransitions";
 
@@ -28,46 +31,70 @@ import { db } from "../firebase-config";
 // };
 
 // type IBookRef = { bookId: string }; kPn2q1L6J9zyd4pasyXi
-
-function Comment(comment, reader) {
-  return (
-    <div>
-      <span>{reader} a commenté:</span>
-      <span>{comment}</span>
-    </div>
-  );
-}
-
-function DeleteButton() {
-  return (
-    // <button className="button" onClick={handleDelete}>
-    `delete`
-    // </button>
-  );
-}
-
 export default function AboutPage() {
-  const { bookId } = useParams();
+  const { bookId, auteur, titre } = useParams();
+  const commentsPath = `books/${bookId}/comments`;
 
   const [comments, setComments] = useState([
     { name: "Loading...", id: "initial" },
   ]);
 
-  useEffect(
-    () =>
-      onSnapshot(collection(db, `books/${bookId}/comments`), (snapshot) => {
-        setComments(
-          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-        );
-      }),
-    []
-  );
+  function Comment({ path }) {
+    const query = collection(db, path);
+    const [docs, loading] = useCollectionData(query);
+
+    return (
+      <div className="text-muted">
+        {loading && "Loading..."}
+        {docs?.map((doc) => (
+          <li key={Math.random()}>
+            <span>{doc.reader} a commenté:</span>
+            <span>
+              <p>{doc.comment}</p>
+              {/* <DeleteButton /> */}
+            </span>
+          </li>
+        ))}
+        <p></p>
+      </div>
+    );
+  }
+
+  function AddNew({ path, reader, comment }) {
+    const name = useRef();
+
+    async function handleSubmit(e) {
+      e.preventDefault();
+
+      const docRefReader = doc(db, path, reader.current.value);
+      // addDoc génère un id automatique au lieu de reader.current.value
+      await setDoc(docRefReader, {
+        reader: reader.current.value,
+        comment: comment.current.value,
+      });
+
+      e.target.reset();
+    }
+
+    return (
+      <li>
+        <form onSubmit={handleSubmit}>
+          <input ref={reader} placeholder="Nom du lecteur" />
+          <input ref={comment} placeholder="Votre commentaire" />
+          <button type="submit">Add</button>
+        </form>
+      </li>
+    );
+  }
+
+  function DeleteButton() {
+    return <button className="button">Supprimer</button>;
+  }
 
   const handleNewComment = async () => {
-    const reader = prompt("Who are you");
-    const comment = prompt("Enter a comment");
-
-    const collectionRef = collection(db, `books/${bookId}/comments`);
+    const reader = prompt("Who are you ?");
+    const comment = prompt("Enter a comment:");
+    const collectionRef = collection(db, commentsPath);
     const payload = { comment, reader };
     const docRef = await addDoc(collectionRef, payload);
 
@@ -76,18 +103,47 @@ export default function AboutPage() {
     // await setDoc(docRef, payload);
   };
 
+  useEffect(() => {
+    onSnapshot(collection(db, `books/${bookId}/comments`), (snapshot) => {
+      setComments(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+  }, []);
+
   return (
     <div className="container p-5">
-      <div className="display-2 text-light"></div>
-      <button className="button" onClick={handleNewComment}>
-        New
-      </button>
-      {comments.map((comment) => (
-        <li key={docRef.id}>
-          <DeleteButton />
-          <a href="#">*</a> <Comment comment={comment} reader={reader} />
-        </li>
-      ))}
+      <div className="d-flex flex-row">
+        <div className="d-flex flex-wrap">
+          <BookCard
+            titre={titre}
+            auteur={auteur}
+            key={bookId}
+            bookId={bookId}
+            commentaires={``}
+          />
+        </div>
+        <div className="d-flex flex-column">
+          <div>{titre}</div>
+          <div>{auteur}</div>
+        </div>
+      </div>
+      <div className="text-light">
+        <button className="button" onClick={handleNewComment}>
+          Entrer un nouveau commentaire
+        </button>
+
+        <div></div>
+        <Comment path={commentsPath} />
+
+        {comments ? (
+          <div>
+            <button className="button" onClick={handleNewComment}>
+              Entrer un nouveau commentaire
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
